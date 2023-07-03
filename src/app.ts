eb34686c239bae47f745c7415d990799a62f2138
Simplystina <62130713+Simplystina@users.noreply.github.com>
2023-07-03 16:10:22 +0100

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
-import express from "express";
+import express, {Request, Response, NextFunction} from "express";
 import config from "config";
 import ConnectDB from "./utils/connectDB";
+import cors from "cors"
 
 const app = express();
+
+
+app.options('*', cors()); // preflight OPTIONS; put before other routes
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 const PORT = config.get("PORT") || 8000;
 ConnectDB()
 
