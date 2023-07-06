b9d6ddbd190c9be19c4dad4976483beba6f949d8
unknown <onosogapelumi@gmail.com>
2023-07-06 14:31:49 +0100

    updates

--- src/schema/invoice.schema.ts
+++ src/schema/invoice.schema.ts
@@ -1,42 +1,47 @@
-import {object, string, TypeOf, date, number, boolean} from 'zod'
+import { object, string, TypeOf, date, number, boolean } from "zod";
 
 export const createInvoiceSchema = object({
-    body : object({
-        invoiceName: string({
-            required_error: "Invoice requires a name"
-        }),
-        issueDate: string({
-            required_error: "Invoice issuedate is required",
-          }),
-        invoiceDesc :string().optional(),
-        dueDate: string({
-            required_error: "Invoice requires a due date",
-          }),
-        amount: number({
-            required_error:"Invoice requires an amount"
-        }),
-        draft: boolean().default(false).optional(),
-        hasClientPaid: boolean().default(false).optional(),
-        paymentReference: string().optional(),
-    })
-})
-
-
-export const updateInvoiceSchema =object({
-    body : object({
-        invoiceName: string().optional(),
-        issueDate: string().optional(),
-        invoiceDesc :string().optional(),
-        dueDate: string().optional(),
-        amount: number().optional(),
-        draft: boolean().optional(),
-        hasClientPaid: boolean().optional(),
-        paymentReference: string().optional(),
+  body: object({
+    invoiceName: string({
+      required_error: "Invoice requires a name",
     }),
-    params: object({
-        id: string()
-    })
-})
+    issueDate: string({
+      required_error: "Invoice issuedate is required",
+    }),
+    invoiceDesc: string().optional(),
+    dueDate: string({
+      required_error: "Invoice requires a due date",
+    }),
+    amount: number({
+      required_error: "Invoice requires an amount",
+    }),
+    draft: boolean().default(false).optional(),
+    hasClientPaid: boolean().default(false).optional(),
+    paymentReference: string().optional(),
+    userId: string({
+      required_error: "user Id is Required",
+    }),
+    clientId: string({
+      required_error: "client id is required",
+    }),
+  }),
+});
 
-export type CreateInvoiceInput = TypeOf<typeof createInvoiceSchema>["body"]
-export type UpdateInvoiceInput = TypeOf<typeof updateInvoiceSchema>
+export const updateInvoiceSchema = object({
+  body: object({
+    invoiceName: string().optional(),
+    issueDate: string().optional(),
+    invoiceDesc: string().optional(),
+    dueDate: string().optional(),
+    amount: number().optional(),
+    draft: boolean().optional(),
+    hasClientPaid: boolean().optional(),
+    paymentReference: string().optional(),
+  }),
+  params: object({
+    id: string(),
+  }),
+});
+
+export type CreateInvoiceInput = TypeOf<typeof createInvoiceSchema>["body"];
+export type UpdateInvoiceInput = TypeOf<typeof updateInvoiceSchema>;
