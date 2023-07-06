835d0828af615da02b50ba3a3d53a9fe6f9e6e58
unknown <onosogapelumi@gmail.com>
2023-07-06 15:31:04 +0100

    updates

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,41 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, { Request, Response, NextFunction } from "express";
 import config from "config";
 import ConnectDB from "./utils/connectDB";
-import cors from "cors"
+import cors from "cors";
 import { useTreblle } from "treblle";
 
 import errorMiddleware from "./middlewares/error.middleware";
-import authMiddleware from "./middlewares/auth.middleware"
+import authMiddleware from "./middlewares/auth.middleware";
 // import { rateLimiter } from "./middlewares/rateLimiter.middleware";
-import compression from "compression"
+import compression from "compression";
 
 //Import routes
-import userRoutes from "./routes/user.routes"
-import clientRoutes from "./routes/client.routes"
+import userRoutes from "./routes/user.routes";
+import clientRoutes from "./routes/client.routes";
 import invoiceRoutes from "./routes/invoice.routes";
-import authRoutes from "./routes/auth.routes"
-import googleauth from "./routes/googleauth.routes"
+import authRoutes from "./routes/auth.routes";
+import googleauth from "./routes/googleauth.routes";
 
 const app = express();
 
-
 //enable cors
-app.options('*', cors()); // preflight OPTIONS; put before other routes
+app.options("*", cors()); // preflight OPTIONS; put before other routes
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
 const PORT = config.get("PORT") || 3000;
 
-ConnectDB()
+ConnectDB();
 const shouldCompress = (req: Request, res: Response) => {
-  if (req.headers['x-no-compression']) {
+  if (req.headers["x-no-compression"]) {
     // Will not compress responses, if this header is present
     return false;
   }
@@ -41,39 +43,36 @@ const shouldCompress = (req: Request, res: Response) => {
   return compression.filter(req, res);
 };
 
-app.use(compression({
-  filter: shouldCompress,
-  threshold: 0
-}))
+app.use(
+  compression({
+    filter: shouldCompress,
+    threshold: 0,
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 //enable rate Limit
 // app.use(rateLimiter)
 
-
 //set up treblle
 useTreblle(app, {
-    apiKey: config.get('TREBLLE_API_KEY'),
-    projectId: config.get('TREBLLE_PROJECT_ID'),
-  })
+  apiKey: config.get("TREBLLE_API_KEY"),
+  projectId: config.get("TREBLLE_PROJECT_ID"),
+});
 
-  app.get("/", (req: Request, res: Response) => {
-    return res.status(200).json({
-      message: "Welcome!"
-    })
-  })
+app.get("/", (req: Request, res: Response) => {
+  return res.status(200).json({
+    message: "Welcome!",
+  });
+});
 
-app.use("/api/v1/users", userRoutes)
-app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/googleauth", googleauth)
-app.use("/api/v1/clients", authMiddleware, clientRoutes)
-app.use("/api/v1/invoice", authMiddleware, invoiceRoutes)
-
-
-
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/auth", authMiddleware, authRoutes);
+app.use("/api/v1/googleauth", googleauth);
+app.use("/api/v1/clients", authMiddleware, clientRoutes);
+app.use("/api/v1/invoice", authMiddleware, invoiceRoutes);
 
 app.listen(PORT, () => console.log(`App Listening at port ${PORT}`));
-
