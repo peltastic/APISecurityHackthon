b88ee6141445dddf4edf511da1d2094346aa42d0
unknown <onosogapelumi@gmail.com>
2023-07-06 12:09:33 +0100

    updated configs

--- src/controllers/googleauth.controller.ts
+++ src/controllers/googleauth.controller.ts
@@ -1,40 +1,40 @@
 import { Request, Response, NextFunction } from "express";
-import {  CreateUser,  } from "../services/users.service";
-import { OAuth2Client } from 'google-auth-library';
-import config from "config"
+import { CreateUser } from "../services/users.service";
+import { OAuth2Client } from "google-auth-library";
+import config from "config";
 
 import UserModel from "../models/users.model";
 
 const client = new OAuth2Client(config.get("GOOGLE_CLIENT_ID"));
-const clientId = config.get("GOOGLE_CLIENT_ID")
+const clientId = config.get("GOOGLE_CLIENT_ID");
 
-
-const initiateGoogleAuth = async(
+const initiateGoogleAuth = async (
   req: Request,
   res: Response,
   next: NextFunction
-)=>{
- 
-  const url = client.generateAuthUrl({
-    scope: 'email profile', 
+) => {
+  try {
+    const url = client.generateAuthUrl({
+      scope: "email profile",
 
-    redirectUri: config.get("GOOGLE_AUTH_REDIRECT_URL"),
-  });
-  res.redirect(url);
-}
+      redirectUri: config.get("GOOGLE_AUTH_REDIRECT_URL"),
+    });
+    return res.redirect(url);
+  } catch (e) {
+    next(e);
+  }
+};
 
-const handleSuccessfulAuth = async(
+const handleSuccessfulAuth = async (
   req: Request,
   res: Response,
   next: NextFunction
-)=>{
+) => {
   const { code } = req.query;
 
   try {
-  
     const { tokens } = await client.getToken(code as string);
 
-   
     const ticket = await client.verifyIdToken({
       idToken: tokens.id_token!,
       audience: clientId as string,
@@ -43,21 +43,24 @@ const handleSuccessfulAuth = async(
     const userId = payload?.sub;
     const email = payload?.email;
     const firstName = payload?.given_name;
-  
+
     const existingUser = await UserModel.findOne({ email });
-    if(existingUser){
-      return res.status(200).json({success: true, message:"Authentication Successful"})
+    if (existingUser) {
+      return res
+        .status(200)
+        .json({ success: true, message: "Authentication Successful" });
     }
     await CreateUser({
-      email : email!,
-      name :firstName!,
-      password:"GoogleAuth"
-    
+      email: email!,
+      name: firstName!,
+      password: "GoogleAuth",
     });
-    return res.status(200).json({success: true, message:"Authentication Successful"})
+    return res
+      .status(200)
+      .json({ success: true, message: "Authentication Successful" });
   } catch (e) {
-    next(e)
+    next(e);
   }
-}
+};
 
-export {handleSuccessfulAuth, initiateGoogleAuth}
+export { handleSuccessfulAuth, initiateGoogleAuth };
